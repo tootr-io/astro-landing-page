0567706e9a4c6cb2b6ec9a6a93e6370330764018
Port25 Peon <jbolton@dev.port25.cc>
2022-09-21 12:53:17 -0600

    sdd

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
+import cloudflare from "@astrojs/cloudflare";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://astro-moon-landing.netlify.app/",
   integrations: [tailwind()],
   vite: {
     ssr: {
-      external: ["@11ty/eleventy-img", "svgo"],
-    },
+      external: ["@11ty/eleventy-img", "svgo"]
+    }
   },
+  output: "server",
+  adapter: cloudflare()
 });
